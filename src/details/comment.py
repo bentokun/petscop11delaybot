b790b111fafd95612e7af869ff5bf71b0d2a28b2
Do Trong Thu <fewdspuckrito@gmail.com>
2017-07-24 14:35:47 +0700

    Fixing tab issues

--- src/details/comment.py
+++ src/details/comment.py
@@ -6,10 +6,10 @@
 #	modification, are permitted provided that the following conditions are met:
 
 #	1. Redistributions of source code must retain the above copyright notice, this
-#   list of conditions and the following disclaimer.
+#	list of conditions and the following disclaimer.
 #	2. Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
+#	this list of conditions and the following disclaimer in the documentation
+#	and/or other materials provided with the distribution.
 
 #	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 #	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -26,8 +26,8 @@
 
 #	comment.py
 
-#   This file is a part of the Petscop11DelayBot
-#   Copyright (c) 2017 Do Trong Thu
+#	This file is a part of the Petscop11DelayBot
+#	Copyright (c) 2017 Do Trong Thu
 
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
