883debef634fd98adda24c18713ed34faddd23fd
Do Trong Thu <fewdspuckrito@gmail.com>
2017-08-11 14:22:27 +0700

    Fixing undb

--- src/details/comment.py
+++ src/details/comment.py
@@ -46,31 +46,96 @@ import praw
 import re
 import bmemcached
 
+from utils import *
+from random import randint
+
 def update_date(database, subreddit, date):
 	for comment in subreddit.comments(limit=None):
 			if re.search("Petscop 11", comment.body, re.IGNORECASE):
 				if not database.get(comment.id) and not database.get(comment.author.name):
-					d2 = add_and_get_time(date)
-					dt_str = numToMonth(d2.month) + " " + str(d2.year)
+					
+					if comment.author.name == "Renaaaaaaaaaaaaaaaaa" or comment.author.name == "lonkish" or comment.author.name == "c-d-hogg":
+						date = add_year_and_get_time(date)
+					else:
+						date = add_and_get_time(date)
+					
+					dt_str = numToMonth(date.month) + " " + str(date.year)
+					dt_key = "Petscop11Date"
+					
 					database.set(dt_key, dt_str)
 					
-					reply_mess=("You have mentioned Petscop Eleven (number form). Now Petscop Eleven is estimated for release in {0} {1}! \n\n ^I'm ^a ^bot ^lol. ^This ^bot ^only ^works ^on ^Petscop ^Reddit ^and ^will ^be ^shut ^down ^on ^26/7/2017 ^or ^sooner. ^If ^you ^feel ^that ^I'm ^annoying ^in ^three ^days, ^you ^can ^block ^me ^by ^inbox ^or ^comment: ^!BLOCK. ^If ^you ^want ^to ^unblock ^me, ^send: ^!UNBLOCK. ^Seriously, ^I'm ^a ^copy ^of ^/u/SeriouslyWhenIsHL3.").format(numToMonth(d2.month), d2.year)
+					if comment.author.name == "c-d-hogg":
+						reply_mess = "Paul detected, delay 1 more year. If Paul want things to be normal again, contact /u/palaskowitz or /u/naul_is_my_lover. \n\n *** \n\n ^I'm ^a ^bot ^lol. ^This ^bot ^only ^works ^on ^Petscop ^Reddit. ^If ^you ^feel ^that ^I'm ^annoying ^, ^you ^can ^block ^me ^by ^inbox ^or ^comment: ^!BLOCK. ^If ^you ^want ^to ^unblock ^me, ^send: ^!UNBLOCK. ^If ^mods ^want ^to ^stop ^this ^bot, ^contact ^/u/palaskowitz ^or ^/u/naul_is_my_lover."
+					elif comment.author.name == "Renaaaaaaaaaaaaaaaaa":
+						reply_mess = ("Jake Paul detected, delay Petscop 1 year. Now Petscop Eleven is estimated for release in {0} {1}. \n\n *** \n\n ^I'm ^a ^bot ^lol. ^This ^bot ^only ^works ^on ^Petscop ^Reddit. ^If ^you ^feel ^that ^I'm ^annoying ^, ^you ^can ^block ^me ^by ^inbox ^or ^comment: ^!BLOCK. ^If ^you ^want ^to ^unblock ^me, ^send: ^!UNBLOCK. ^If ^mods ^want ^to ^stop ^this ^bot, ^contact ^/u/palaskowitz ^or ^/u/naul_is_my_lover.").format(numToMonth(date.month), date.year)
+					elif comment.author.name == "lonkish":
+						reply_mess = "Russian detected, afaird of Putin, delay Petscop 1 year. Now Petscop Eleven is estimated for release in {0} {1}. \n\n *** \n\n ^I'm ^a ^bot ^lol. ^This ^bot ^only ^works ^on ^Petscop ^Reddit. ^If ^you ^feel ^that ^I'm ^annoying ^, ^you ^can ^block ^me ^by ^inbox ^or ^comment: ^!BLOCK. ^If ^you ^want ^to ^unblock ^me, ^send: ^!UNBLOCK. ^If ^mods ^want ^to ^stop ^this ^bot, ^contact ^/u/palaskowitz ^or ^/u/naul_is_my_lover.".format(numToMonth(date.month), date.year)
+					else:
+						reply_mess=("Oh no, what have you done you ding dong? You have mentioned Petscop Eleven. Now Petscop Eleven is now estimated for release in {0} {1}! \n\n *** \n\n ^I'm ^a ^bot ^lol. ^This ^bot ^only ^works ^on ^Petscop ^Reddit. ^If ^you ^feel ^that ^I'm ^annoying ^, ^you ^can ^block ^me ^by ^inbox ^or ^comment: ^!BLOCK. ^If ^you ^want ^to ^unblock ^me, ^send: ^!UNBLOCK. ^If ^mods ^want ^to ^stop ^this ^bot, ^contact ^/u/palaskowitz ^or ^/u/naul_is_my_lover.").format(numToMonth(date.month), date.year)
 					
 					try:
 						comment.reply(reply_mess)
 						print("Comment= ", comment.body, " ; CommentID= ", comment.id, "replied")
-						mc.set(comment.id, "True")
+						database.set(comment.id, "True")
 					except:
 						print("Error happened with comment id: ", comment.id)
 						
-						d2=sub_and_get_time(date)
-						dt_str = numToMonth(d2.month) + " " + str(d2.year)
-						mc.set(dt_key, dt_str)
+						date=sub_and_get_time(date)
+						dt_str = numToMonth(date.month) + " " + str(date.year)
+						database.set(dt_key, dt_str)
 						pass
 					
 					pass
 				else:
 					print("Comment= ", comment.body, " ; CommentID= ", comment.id, "replied")
+				pass
+			elif re.search("bad bot", comment.body, re.IGNORECASE) and comment.parent().author.name == "palaskowitz":
+				print("Some one say bad bot: ")
+			
+				if not database.get(comment.id) and not database.get(comment.author.name):
+						
+					reply_mess=["I'm legit crying rn", "Kys", "Sorry, {0}".format(comment.author.name), "E N G L A N D I S M Y C I T Y", "/u/SeriouslyWhenIsHL3"]
+						
+					try:
+						comment.reply(reply_mess[randint(0, 5)])
+						print("Comment= ", comment.body, " ; CommentID= ", comment.id, "replied")
+						database.set(comment.id, "True")
+					except:
+						print("Error happened with comment id: ", comment.id)
+							
+						pass
+						
+					pass
+				else:
+					print("Comment= ", comment.body, " ; CommentID= ", comment.id, "replied")
+				pass
+			elif re.search("c-d-hogg", comment.body, re.IGNORECASE):
+				if not database.get(comment.id) and not database.get(comment.author.name):
+					date = sub_and_get_time(date)
+					dt_str = numToMonth(date.month) + " " + str(date.year)
+					dt_key = "Petscop11Date"
+					
+					database.set(dt_key, dt_str)
+						
+					reply_mess="By mentioning Paul you have earlier the release date of Petscop Eleven by 1 month. Now Petscop Eleven is estimated for release in {0} {1}! Yayyyy! \n\n *** \n\n ^I'm ^a ^bot ^lol. ^This ^bot ^only ^works ^on ^Petscop ^Reddit. ^If ^you ^feel ^that ^I'm ^annoying ^, ^you ^can ^block ^me ^by ^inbox ^or ^comment: ^!BLOCK. ^If ^you ^want ^to ^unblock ^me, ^send: ^!UNBLOCK. ^If ^mods ^want ^to ^stop ^this ^bot, ^contact ^/u/palaskowitz ^or ^/u/naul_is_my_lover.".format(numToMonth(date.month), date.year)
+						
+					try:
+						comment.reply(reply_mess)
+						print("Comment= ", comment.body, " ; CommentID= ", comment.id, "replied")
+						database.set(comment.id, "True")
+					except:
+						print("Error happened with comment id: ", comment.id)
+							
+						date=add_and_get_time(date)
+						dt_str = numToMonth(date.month) + " " + str(date.year)
+						database.set(dt_key, dt_str)
+						pass
+						
+					pass
+				else:
+					print("Comment= ", comment.body, " ; CommentID= ", comment.id, "replied")
+				pass
+	
 			
 	return date
 	
